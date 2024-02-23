20fcf45589683ce1e6311c7ef78aa85fd9f1e348
0xRayan <rayan.seghrouchni@jobtrek.ch>
2024-02-23 14:48:22 +0100

    refactor: Added navbar

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+
+
+
+<nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-5 p-7">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="skilltreklogo.svg" class="h-30" alt="Skilltrek Logo" />
+            <span class="self-center text-5xl font-semibold whitespace-nowrap dark:text-white">SkillTrek</span>
+        </a>
+    </div>
+</nav>
+
+</body>
+</html>
