33930e139d1253814a154a1e345a3b9023a7c74f
0xRayan <rayan.seghrouchni@jobtrek.ch>
2024-02-23 16:17:15 +0100

    feat: Add home title card

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title', "SkillTrek - Home")
+
+@section('body')
+
+    <a
+        class="block max-w-lg  mt-10 mx-10 p-10 border border-gray-200 shadow hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-700" style="background-color: black;">
+
+        <h1 class="mb-7 text-5xl font-bold tracking-tight text-white dark:text-white">A forms creator <br> website.</h1>
+        <h3 class="text-3xl font-normal text-white dark:text-white">Fast and smart</h3>
+    </a>
+
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,24 +1,26 @@
 <!doctype html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <title>@yield('title')</title>
 </head>
 <body>
 
 
 
 <nav class="border-gray-200" style="background-color: black !important; dark:bg-gray-800 dark:border-gray-700;">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-5 p-7">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="skilltreklogo.svg" class="h-30" alt="Skilltrek Logo" />
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between p-10">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="skilltreklogo.svg" class="w-20" alt="Skilltrek Logo" />
             <span class="self-center text-5xl font-semibold whitespace-nowrap dark:text-white">SkillTrek</span>
         </a>
     </div>
 </nav>
 
 
+@yield('body')
 
 </body>
 </html>
