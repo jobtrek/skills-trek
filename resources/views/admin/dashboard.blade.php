282f9d00b2b9992e386c48fb067c9c4816da0565
0xRayan <rayan.seghrouchni@jobtrek.ch>
2024-02-27 14:43:24 +0100

    feat: Added text and layout to dashboard page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,5 +5,9 @@
 @section('pagetitle', "Dashboard")
 
 @section('body')
-
+    <div class="text-center mt-20">
+        <span class="font-bold text-3xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing </span>
+    </div>
 @endsection
