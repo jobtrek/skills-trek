4dc406da378a59ca0c7c3d2c5244066f179375bd
0xRayan <142514766+0xRayan14@users.noreply.github.com>
2024-02-27 14:36:26 +0100

    Admin groups page (#17)
    * Delete .idea directory
    
    * fix: Adjusted routes
    
    * fix: Adjusted favicon
    
    * fix: Adjusted titles
    
    * style: Change homepage background size
    
    * style: Change homepage background size
    
    * feat: Made groups file
    
    * fix: Admin groups route
    
    * refactor: Changed users to groups
    
    * feat: Add new form button

---
+++ resources/views/admin/groups.blade.php
@@ -0,0 +1,12 @@
+@extends('sidebar')
+
+@section('title', "SkillTrek - Groups")
+
+@section('pagetitle', "Groups management")
+
+@section('body')
+    <a href="#">
+        <img src="{{ asset('/icons/newform.svg') }}" class="fixed right-20" alt=""/>
+    </a>
+
+@endsection
