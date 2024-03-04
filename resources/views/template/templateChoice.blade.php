bef720c7e65eb035e75f7a42befa82bd7e53c7d1
kylian <kylianclerc@fedora>
2024-03-04 14:14:39 +0100

    feat: create template choice

---
+++ resources/views/template/templateChoice.blade.php
@@ -0,0 +1,8 @@
+@vite('resources/css/app.css')
+
+
+
+<div class="flex items-center mt-2">
+    <input type="checkbox" class="mr-2">
+    <input type="text" class="mr-2 p-2 border rounded bg-gray-200" placeholder="Réponse 1">
+</div>
