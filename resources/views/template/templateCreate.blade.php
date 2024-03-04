8c1c633f1ce31eeaf47c5622b460059bce70d117
kylian <kylianclerc@fedora>
2024-03-04 14:14:49 +0100

    feat: create template create

---
+++ resources/views/template/templateCreate.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layout')
+
+@section('title', "SkillTrek - success")
+@section('body')
+
+<body class="bg-gray-200">
+<div class="flex items-start justify-center h-screen">
+    @include('template.templateTitle')
+
+
+
+    <form  method="post">
+        @csrf
+
+    </form>
+</div>
+
+<button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 my-custom-button-class">Soumettre</button>
+
+
+</body>
+
+@endsection
